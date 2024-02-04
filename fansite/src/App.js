0d5572825ff9e1ca0ade8c141835338109b00203
yeon <test2@test2.com>
2024-02-04 23:32:09 +0900

    context

--- fansite/src/App.js
+++ fansite/src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import Router from './Components/Shared/Router';
+import GlobalStyle from './GlobalStyle';
+// import Home from './Components/Pages/Home';
+// import Form from './Components/common/Form';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<>
+			<GlobalStyle />
+			{/* <Home /> */}
+			<Router />
+		</>
+	);
 }
 
 export default App;
