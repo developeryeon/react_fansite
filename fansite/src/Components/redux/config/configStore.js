1e3058aee2d2c4f9b479dd667c9a8141e0b0d5a7
yeon <test2@test2.com>
2024-02-05 13:05:13 +0900

    props-drilling

--- fansite/src/Components/redux/config/configStore.js
+++
@@ -1,9 +0,0 @@
-import { createStore } from 'redux';
-import { combineReducers } from 'redux';
-
-const rootReducer = combineReducers({});
-const store = createStore(rootReducer);
-
-export default store;
-
-//const rootReducer = combineReducers({ todos, auth }) === const todos = useSelector((state) => state.todos )
